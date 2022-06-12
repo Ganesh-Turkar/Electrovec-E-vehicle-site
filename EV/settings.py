52a4bcf0404252bbaadbd790294908f44b63370f
Ganesh-Turkar <ganeshturkar6@gmail.com>
2022-06-12 18:40:53 +0530

    checking commit

--- EV/settings.py
+++ EV/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+# git commit -a -m "massage what we commiting"
 from pathlib import Path
 import os
 
